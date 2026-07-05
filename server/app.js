a0e5b16a5442f2d0107dc8e64aaaef5d35ad728c
Dipu Gupta <deepukusaura@gmail.com>
2026-07-05 12:46:15 +0530

    Added final backend polishing

--- server/app.js
+++ server/app.js
@@ -16,10 +16,12 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+// Health Check Route
 app.get("/", (req, res) => {
   res.status(200).json({
     success: true,
-    message: "DrawVerse API is running ",
+    message: "Skribbl Clone Backend is Running 🚀",
+    environment: env.NODE_ENV,
   });
 });
 
